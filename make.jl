a6540c12dccb673b2cdebd223d0218ce5808495f
Joseph Broz <brozj11@gmail.com>
2020-09-09 02:22:51 -0700

    initial setup

---
+++ make.jl
@@ -0,0 +1,16 @@
+using Documenter, IonSim 
+
+makedocs(
+    modules = [IonSim],
+    checkdocks = :exports,
+    sitename = "IonSim.jl",
+    format = Documenter.HTML(
+        canonical = "https://docs.ionsim.org",
+        sidebar_sitename = false,
+        assets = [
+                asset("assets/js/link-back.js", class=:js, islocal=true),
+                asset("assets/small-logo.png", class=:ico, islocal = true)
+            ],
+        edit_link = nothing
+    )
+)
